2e75662a02a1371c52d56b60a168c9366a1101db
Santeri Uusitalo <vs.uusitalo@gmail.com>
2023-06-15 22:48:50 +0300

    improved styling

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
+    // get elements from html file and save to variables
 let result = document.getElementById("result");
 let searchbutton = document.getElementById("searchbutton");
 let city = document.getElementById("city");
 
-
+    // function to get weather data from api
 let getWeather = () => {
     let cityValue = city.value;
     let url = `https://api.weatherapi.com/v1/current.json?key=${apikey}&q=${cityValue}`;
 
+        // check if city is empty
     if (cityValue == "") {
-        //alert("Please enter a city name");
+        //alert("Please enter a city name"); //alerts browser
         result.innerHTML = `<h3 class="errormsg">Please enter a city name</h3>`;
     }
     else{
@@ -25,12 +27,13 @@ let getWeather = () => {
 
             result.innerHTML = `
             <h2 id="locname">${data.location.name}</h2>
-            <h4 id="condtext">${data.current.condition.text}</h4>
-            <img src=${data.current.condition.icon}></img>
+            <h3 id="condtext">${data.current.condition.text}</h3>
+            <img id=condimg src=${data.current.condition.icon}></img>
             <h1 id="tempnumber">${roundedTemp}°C</h1>
-            <h2 id="loctime">${parsedTime[1]}</h2>
+            <h2 id="loctime">local time: ${parsedTime[1]}</h2>
             `
         })
+            // catch error if data was not found
         .catch((error) => {
             console.log("error");
             result.innerHTML = `<h3 class="errormsg">City not found</h3>`;
@@ -40,6 +43,7 @@ let getWeather = () => {
 searchbutton.addEventListener("click", getWeather);
 window.addEventListener("load", getWeather);
 
+    // allow user to press enter to search
 city.addEventListener("keydown", function (event){
     if (event.key == "Enter"){
         event.preventDefault();
