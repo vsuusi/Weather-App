7f70444df93c5b497e3090086fb0c2e3cce90b0c
Santeri Uusitalo <vs.uusitalo@gmail.com>
2023-06-15 19:44:13 +0300

    removed key.js from repo

--- key.js
+++
@@ -1,4 +0,0 @@
-// key = "991fce5c3784bbcd49e2a2ac4e7aed41";
-// let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${key}&units=metric`;
-
-apikey = "3c1123ef98934a88bdc154758231506 ";
